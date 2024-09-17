461887cb6b3442e561471d93a51fa1402bf41243
DenzelFoxUA <canemidnight21@gmail.com>
2024-09-17 20:22:18 +0300

    Add project files.

---
+++ EnumeratorTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace EnumeratorTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var textCollection = new SomeCustomWordCollection(new[]{ "word1", "word2", "word3", "word4", "word5" });
+
+            foreach (var s in textCollection)
+            {
+                Console.WriteLine($"{s}, ");
+            }
+        }
+    }
+}
---
+++ EnumeratorTest/SomeCustomCollectionEnumerator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumeratorTest
+{
+    public class SomeCustomWordCollection : IEnumerable
+    {
+        private string[] _words;
+
+        public SomeCustomWordCollection(string[] words)
+        {
+            _words = words;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            var enumerator = new SomeCustomCollectionEnumerator(_words);
+            while (enumerator.MoveNext())
+            {
+                yield return enumerator.Current;
+            }
+        }
+    }
+
+
+    public class SomeCustomCollectionEnumerator : IEnumerator
+    {
+        private object[] _collection;
+        private int _currentIndex;
+        private int _startingPoint = -1;
+        private int _size;
+
+        private SomeCustomCollectionEnumerator()
+        {
+            _size = 0;
+            _collection = new object[_size];
+        }
+
+        public SomeCustomCollectionEnumerator(object [] collection)
+        {
+            _currentIndex = _startingPoint;
+            _size = collection.Length;
+            _collection = collection;
+        }
+
+        public int Size { get => _collection.Length; private set => _size = value; }
+
+        public object Current => GetObject(_currentIndex);
+
+        public bool MoveNext()
+        {
+            _currentIndex++;
+            return _currentIndex < Size;
+        }
+
+        public void Reset()
+        {
+            _currentIndex = _startingPoint;
+        }
+
+        private object GetObject(int index)
+        {
+
+            try
+            {
+                return _collection[index];
+            }
+            catch(IndexOutOfRangeException ex)
+            {
+                string msg = ex.Message;
+                throw new IndexOutOfRangeException(msg);
+            }
+            catch(Exception ex)
+            {
+               string msg = ex.Message;
+               throw new Exception(msg);
+            }
+        }
+
+    }
+}
